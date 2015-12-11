4a0f71af233687e7759f425a82388c964614dd9a
Vadim Macagon <vadim.macagon@gmail.com>
2015-12-11 23:37:11 +0700

    Add the source files

---
+++ typings/babel-traverse.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for babel-traverse v6.3.17
+// Project: https://github.com/babel/babel/tree/master/packages/babel-traverse
+// This is just a small subset of what's actually available.
+
+interface NodePath<T> {
+  node: T;
+
+  get<T>(path: string): NodePath<T>;
+  /**
+   * Check if the node is a member expression matching the given pattern.
+   *
+   * For example, the pattern `Polymer.Base` will match the AST for `Polymer.Base` and the AST
+   * for `Polymer['Base']`.
+   */
+  matchesPattern(pattern: string, partialMatch?: boolean): boolean;
+  /** Remove the node from the AST. */
+  remove(): void;
+}
---
+++ typings/babel-types.d.ts
@@ -0,0 +1,82 @@
+// Type definitions for babel-types v6.3.17
+// Project: https://github.com/babel/babel/tree/master/packages/babel-types
+// Adapted from https://github.com/babel/babel/blob/master/lib/types.js
+// This is just a small subset of what's actually available.
+
+declare class BabelNodeComment {
+  value: string;
+  start: number;
+  end: number;
+  loc: BabelNodeSourceLocation;
+}
+
+declare class BabelNodeBlockComment extends BabelNodeComment {
+  type: "BlockComment";
+}
+
+declare class BabelNodeLineComment extends BabelNodeComment {
+  type: "LineComment";
+}
+
+declare class BabelNodeSourceLocation {
+  start: {
+    line: number;
+    column: number;
+  };
+
+  end: {
+    line: number;
+    column: number;
+  };
+}
+
+declare class BabelNode {
+  leadingComments: Array<BabelNodeComment>;
+  innerComments: Array<BabelNodeComment>;
+  trailingComments: Array<BabelNodeComment>;
+  start: number;
+  end: number;
+  loc: BabelNodeSourceLocation;
+}
+
+declare class BabelNodeCallExpression extends BabelNode {
+  type: "CallExpression";
+  callee: BabelNodeExpression;
+  arguments: Array<any>;
+}
+
+declare class BabelNodeIdentifier extends BabelNode {
+  type: "Identifier";
+  name: any;
+  typeAnnotation: any;
+}
+
+declare class BabelNodeMemberExpression extends BabelNode {
+  type: "MemberExpression";
+  object: BabelNodeExpression;
+  property: any;
+  computed: boolean;
+}
+
+declare class BabelNodeClassBody extends BabelNode {
+  type: "ClassBody";
+  body: any;
+}
+
+declare class BabelNodeClassExpression extends BabelNode {
+  type: "ClassExpression";
+  id: BabelNodeIdentifier;
+  body: BabelNodeClassBody;
+  superClass: BabelNodeExpression;
+  decorators: any;
+  typeParameters: any;
+  superTypeParameters: any;
+}
+
+type BabelNodeExpression = BabelNodeCallExpression | BabelNodeMemberExpression;
+
+declare interface BabelTypes {
+  isIdentifier(node: Object, opts?: Object): node is BabelNodeIdentifier;
+  isMemberExpression(node: Object, opts?: Object): node is BabelNodeMemberExpression;
+  isCallExpression(node: Object, opts?: Object): node is BabelNodeCallExpression;
+}
